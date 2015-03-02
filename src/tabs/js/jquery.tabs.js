beba95178596990fab90d1f1fb676646ac160b9f
Frederik Aerts <frederik.aerts@kunstmaan.be>
2015-03-02 12:35:34 +0100

    functional jquery setup

---
+++ src/tabs/js/jquery.tabs.js
@@ -0,0 +1,75 @@
+/* ==========================================================================
+   Toggle
+
+   Initialize:
+   cargobay.tabs.init();
+
+   Support:
+   Latest Chrome
+   Latest FireFox
+   Latest Safari
+   IE9 and up
+   ========================================================================== */
+
+
+var cargobay = cargobay || {};
+
+cargobay.tabs = (function($, window, undefined) {
+
+    var init, activateTabs, updatePanes;
+
+    // Config
+    var defaultAnimationDuration = 150,
+        animationDuration = 0,
+        tabClass = 'js-tab',
+        tabClassActive = 'tab--active',
+        paneClassActive = 'tab-pane--active';
+
+
+    // Init
+    init = function() {
+        activateTabs();
+    };
+
+
+    // Main toggle function
+    activateTabs = function() {
+        $('.' + tabClass).on('click touchstart mousedown', function(e) {
+            e.preventDefault();
+        }).on('touchend mouseup', function() {
+            var $this = $(this),
+                $target = ($this.data('target') !== undefined) ? $($this.data('target')) : $($this.children('a').first().attr('href')),
+                currentTargetIsActive = $target.hasClass(tabClassActive);
+
+            console.log($this.attr('href'));
+
+            // Check if custom animation duration has been set.
+            animationDuration = ($this.data('duration') !== undefined) ? $this.data('duration') : defaultAnimationDuration;
+
+
+            if(currentTargetIsActive) {
+               // Target is active, so return
+               return false;
+
+            } else {
+                // Update tabs
+                $this.siblings().removeClass(tabClassActive);
+                $this.addClass(tabClassActive);
+
+                // Update panes
+                updatePanes($target);
+            }
+        });
+    };
+
+    // Show target pane
+    updatePanes = function($target) {
+        $target.siblings().removeClass(paneClassActive);
+        $target.addClass(paneClassActive);
+    };
+
+    return {
+        init: init
+    };
+
+}(jQuery, window));
